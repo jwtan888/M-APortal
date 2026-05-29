b485da4edacbe8946b946bcdab984d1afccb4d98
jwtan888 <jwtan@sig.com.my>
2026-05-29 12:27:13 +0800

    Add files via upload

---
+++ template-ai/config.js
@@ -0,0 +1,4 @@
+window.PATCH_TEMPLATE_CONFIG = {
+  powerAutomateTrainingUrl: "https://defaultb4f081a089004baaa6a8ff79312af2.61.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/58f9998b29464cee93a3407b10c10045/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=O5vvkecIbOBe75nYw-kfgk6MIRuE1E8bYz2tyl5Tyug",
+  powerAutomateReadUrl: "https://defaultb4f081a089004baaa6a8ff79312af2.61.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/dfb54b99731b45c49fce9b3f7c34c8c1/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=sQzme_zFhLPOwKYj5COWalJ0a7_v1us3u_Jx0cn7is8",
+};
